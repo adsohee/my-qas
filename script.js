18f36b16babcad3a14d323fb118cff9720cad6c0
adsohee <adsohee@gmail.com>
2026-06-09 11:25:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const logoContainer = document.getElementById("logoContainer");
+const mainScreen = document.getElementById("mainScreen");
 
+logoContainer.addEventListener("click", () => {
+
+  // 로고 사라짐
+  logoContainer.classList.add("hide");
+
+  // 메인 화면 등장
+  setTimeout(() => {
+    mainScreen.classList.add("show");
+  }, 400);
+
+});
