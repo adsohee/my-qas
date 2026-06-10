33f4919aaa560a2b7f564fa3a46106dbba28f8df
adsohee <adsohee@gmail.com>
2026-06-10 10:43:35 +0900

    Update script.js 추가 PWA

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
 const mainScreen = document.getElementById("mainScreen");
 
 mainScreen.classList.add("show");
+
+let deferredPrompt;
+
+const installButton =
+  document.getElementById("installButton");
+
+/* 설치된 상태면 숨김 */
+
+if (
+  window.matchMedia("(display-mode: standalone)").matches
+) {
+
+  installButton.style.display = "none";
+
+}
+
+/* 설치 가능 이벤트 */
+
+window.addEventListener(
+  "beforeinstallprompt",
+  (e) => {
+
+    e.preventDefault();
+
+    deferredPrompt = e;
+
+    installButton.style.display = "flex";
+
+  }
+);
+
+/* 버튼 클릭 */
+
+installButton?.addEventListener(
+  "click",
+  async () => {
+
+    if (!deferredPrompt) return;
+
+    deferredPrompt.prompt();
+
+    await deferredPrompt.userChoice;
+
+    deferredPrompt = null;
+
+    installButton.style.display = "none";
+
+  }
+);
